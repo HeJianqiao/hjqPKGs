468ad1f76a3db395ea1cbf949a37e989dd17f836
hejianqiao <507595917@qq.com>
2019-06-25 23:12:46 +0800

    init file

---
+++ ContentDeal.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hejianqiao
+ * Date: 2019/6/25
+ * Time: 11:07 PM
+ */
+
+class ContentDeal
+{
+
+    public function chkSenWord():bool
+    {
+        echo 'chk sen word.';
+        return false;
+    }
+}
